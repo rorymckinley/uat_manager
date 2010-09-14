bef383ddbf711b1b49439e0b32f2d25d3a68d0e4
Rory McKinley <rorymckinley@gmail.com>
2010-09-14 21:50:50 +0200

    Controller now returns a representation of an AcceptanceTest

--- app/controllers/acceptance_tests_controller.rb
+++ app/controllers/acceptance_tests_controller.rb
@@ -2,4 +2,7 @@ class AcceptanceTestsController < ApplicationController
   def create
     render :text => AcceptanceTest.create(params[:acceptance_test]).id, :status => :ok
   end
+  def show
+    render :json => AcceptanceTest.find(params[:id]), :status => :ok
+  end
 end
--- spec/controllers/acceptance_tests_controller_spec.rb
+++ spec/controllers/acceptance_tests_controller_spec.rb
@@ -7,7 +7,14 @@ describe AcceptanceTestsController do
   it "should create an instance of an AcceptanceTest" do
     post :create, :acceptance_test => { :name => 'Testing creation of an AcceptanceTest', :context => 'UAT Manager', :starting_point => 'Start Page' }
     response.should be_success
-    AcceptanceTest.all.size.should == 1
+    AcceptanceTest.count.should == 1
     AcceptanceTest.find(response.body.to_i).should_not be_nil
+  end
+  it "should return data for an instance of an AcceptanceTest" do
+    post :create, :acceptance_test => { :name => 'Testing creation of an AcceptanceTest', :context => 'UAT Manager', :starting_point => 'Start Page' }
+    created_id = response.body.to_i
+    get :show, :id => created_id
+    response.should be_success
+    response.body.should == AcceptanceTest.find(created_id).to_json
   end
 end
